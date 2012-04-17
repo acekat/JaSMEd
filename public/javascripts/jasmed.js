0e39feb0d05fe01c306b587ea0d3f023ec93d989
Valentin CASSAT <ace.no.val@gmail.com>
2012-04-17 17:12:26 +0200

    Added a Backbone structure example

---
+++ public/javascripts/jasmed.js
@@ -0,0 +1,30 @@
+var jasmed = {
+
+	// Create/Get module
+	module: _.memoize(function(name) {
+		return jasmed.module[name] || {};	
+	})
+}
+
+
+// Treat this as the single entry point into the application.
+$(function() {
+
+	var GridApp = jasmed.module("grid");
+
+	// Create the root application Router
+	var Router = Backbone.Router.extend({
+		initialize: function() {
+			console.log('Grid creation...');
+			GridApp.grid = new GridApp.Blocs();
+
+			console.log('GridView creation...');
+			GridApp.gridView = new GridApp.GridView({ collection : GridApp.grid });
+
+			GridApp.grid.add(new GridApp.Bloc());
+		}
+	});
+
+	// Initialize it into the application namespace
+	new Router();
+});

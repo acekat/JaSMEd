abaec1c372cd180d74a8336da04d7ccc89439edf
Thibaud Cummings <thibaud.cummings@gmail.com>
2012-05-01 20:40:48 +0200

    personal gitignore

--- public/javascripts/jasmed.js
+++ public/javascripts/jasmed.js
@@ -1,8 +1,38 @@
-var jasmed = {
+var mediator = (function() {
+	var subscribe = function(channel, fn) {
+		if (!mediator.channels[channel]) mediator.channels[channel] = [];
+		mediator.channels[channel].push({ context: this, callback: fn });
+		return this;
+	},
 
+	publish = function(channel) {
+		if (!mediator.channels[channel]) return false;
+		var args = Array.prototype.slice.call(arguments, 1);
+		for (var i = 0, l = mediator.channels[channel].length; i < l; i++) {
+			var subscription = mediator.channels[channel][i];
+			subscription.callback.apply(subscription.context, args);
+		}
+		return this; //so can chain method calls ?
+	};
+
+	return {
+		channels: {},
+		publish: publish,
+		subscribe: subscribe,
+		installTo: function(obj) {
+			obj.subscribe = subscribe;
+			obj.publish = publish;
+		}
+	};
+})();
+
+var jasmed = {
 	// Create/Get module
 	module: _.memoize(function(name) {
-		return jasmed.module[name] || {};	
+		var m = jasmed.module[name];
+		var newModule = {};
+		mediator.installTo(newModule);
+		return m || newModule;
 	})
 };
 

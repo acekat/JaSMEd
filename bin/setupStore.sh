100b032f48c3dd6bc61287465f1a5db0b83e520b
Acekat <ace.no.val@gmail.com>
2012-09-12 14:18:14 +0200

    fixed unsuccessful attempt to write bash script @thibaud

--- bin/setupStore.sh
+++ bin/setupStore.sh
@@ -1,8 +1,10 @@
-for user in {"thibaud","jaimito","grizix","acekat","berthou","esj"}; do  
-  dir=store/$user
-  if [[ ! -e $dir ]]; then
-    mkdir -p $dir
+#!/bin/bash
+
+for user in "thibaud" "jaimito" "grizix" "acekat" "berthou" "esj"; do  
+	dir=store/$user
+	if [[ ! -e $dir ]]; then
+		mkdir -p $dir
 	elif [[ ! -d $dir ]]; then
-    echo "$dir already exists but is not a directory" 1>&2
+		echo "$dir already exists but is not a directory" 1>&2
 	fi
 done
